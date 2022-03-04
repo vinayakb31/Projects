7474cea82f6e3a2c348b3c35f9021f49a9987cb8
Vinayak <93078680+czariwnl@users.noreply.github.com>
2022-03-05 00:21:29 +0530

    Update and rename Volume of sphere.py to Sphere value calculator.py

---
+++ Python Projects/Sphere value calculator.py
@@ -0,0 +1,28 @@
+# Sphere calculator
+
+from cmath import pi
+import math
+
+r = int(input('Enter the radius of the sphere'))
+
+print("""
+What do you want to calculate?
+1. Surface Area
+2. Volume
+
+""")
+f = int(input('Enter the number'))
+
+if f == 1:
+    res = 4*pi*r**2
+elif f == 2:
+    res = (4/3)*pi*r**3
+
+format_res = "{:.2f}".format(res)
+
+if f == 1:
+    n = 'surface area'
+elif f == 2:
+    n = 'volume'
+
+print('The', n, 'of the sphere is:', format_res)
--- Python Projects/Volume of sphere.py
+++
@@ -1,3 +0,0 @@
-r = int(input("Enter the radius: "))
-vol = 4/3*22/7*r*r*r
-print("Volume of the sphere is", vol)
